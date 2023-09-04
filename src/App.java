5e8157782dc5a8dc64d29866a0932f0c7e9d88aa
Ruben Wilkenson <ruben.wilkenson@elev.ga.ntig.se>
2023-09-04 09:06:13 +0200

    Lagg till summa och subtraktion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 31;
+        int tal2 = 3;
+
+        int summa_addition = tal1 + tal2;
+        int summa_subtraction = tal1 - tal2;
+
+        System.out.println(summa_addition);
+        System.out.println(summa_subtraction);
+
+   
     }
 }
