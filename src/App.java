c6810c732b65b38b8d4384d6d8cfb59bfc69c640
Ruben Wilkenson <ruben.wilkenson@elev.ga.ntig.se>
2023-09-04 09:14:38 +0200

    Lagg till decimaltals kod

--- src/App.java
+++ src/App.java
@@ -1,19 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int tal1 = 31;
+        int tal1 = 30;
         int tal2 = 3;
 
         int summa_addition = tal1 + tal2;
         int summa_subtraction = tal1 - tal2;
 
-        System.out.println(summa_addition);
-        System.out.println(summa_subtraction);
+        System.out.println("30 + 3 = " + summa_addition);
+        System.out.println("30 - 3 = " +summa_subtraction);
 
         int produkt = tal1 * tal2;
         int kvot = tal1 / tal2;
 
-        System.out.println(produkt);
-        System.out.println(kvot);
+        System.out.println("30 * 3 = " + produkt);
+        System.out.println("30 / 3 = " + kvot);
    
         int rest1 = 10%3;
         int rest2 = 7%4;
@@ -21,6 +21,15 @@ public class App {
         System.out.println("resten av 10/3 är " + rest1);
         System.out.println("resten av 7/4 är " + rest2);
 
-        
+        double decimal_tal1 = 5.0;
+        double decimal_tal2 = 23.0;
+
+        double dec_summa_addition = decimal_tal1 + decimal_tal2;
+        double dec_summa_subtraktion = decimal_tal1 - decimal_tal2;
+
+        System.out.println("5.0 + 23.0 = "+ dec_summa_addition);
+        System.out.println("5.0 - 23.0 = "+dec_summa_subtraktion);
+
+
     }
 }
